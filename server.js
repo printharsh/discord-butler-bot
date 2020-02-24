ee8d0ea55ead0b549f9e499e28d38bc9c2b287a2
Harsh Patel <me@mrharshpatel.com>
2020-02-24 16:26:45 -0500

    Created node_package

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./secrets.json');
+var settings = require('./settings.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
@@ -17,23 +21,4 @@ bot.on('ready', function (evt) {
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
-});
+
