3da24d6fd52bc8ca41447a8af7069f56d0247a7a
Harsh Patel <me@mrharshpatel.com>
2020-02-24 16:58:26 -0500

    hopefully done

--- server.js
+++ server.js
@@ -22,3 +22,53 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+rollingUser = 123
+rolling = false
+
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.voiceChannel
+    let oldUserChannel = oldMember.voiceChannel
+  
+    
+    // User has joined voice channel and we're looking to potentially roll!
+    if(!rolling && oldUserChannel === undefined && newUserChannel !== undefined) {
+        let random = Math.random();
+        if (random <= settings.chance){
+            // Time to Roll!
+            rolling = true
+
+            // Will disconnect if user disconnects from channel
+            rollingUser = newMember.id
+
+            newUserChannel.join().then(connection => {
+                // Yay, it worked!
+                logger.info("About to roll, connected to channel.");
+                const dispatcher = connection.playFile('./audio/roll.mp3');
+                dispatcher.on("end", end => {
+                    rolling = false
+                    newUserChannel.leave();
+                });
+                
+                // Nest the listener for checking if user leaves
+                bot.on('voiceStateUpdate', (oldMemberLeaver, newMemberLeaver) => {
+                    let newUserChannelLeave = newMemberLeaver.voiceChannel
+                    if(newUserChannelLeave === undefined){
+                        // User leaves a voice channel
+                        if(newMember.id == rollingUser){
+                            // Means we're rolling so disconnect and set rolling to false
+                            rolling = false
+                            newUserChannel.leave();
+                        }
+                    }
+                });
+
+              }).catch(e => {
+                // Oh no, it errored! Let's log it to console :)
+                logger.error(e);
+              });
+
+        }
+
+    }
+  }
+);
