ee3d40628ceb04c8722bf408608ab56420dd36d2
Harsh Patel <me@mrharshpatel.com>
2020-02-26 19:05:15 -0500

    fixing voiceStatusUpdate

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,4 +1,4 @@
-const logger = require('./logger.js');
+const logger = require('../logger.js');
 
 function roll(random, client, joinedChannel, leftChannel, newMember){
     let chance = client.settings.chances.roll
