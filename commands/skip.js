013fc5c1fe1c5977ffde44dc8dfbafa97ea3fd14
Harsh Patel <harsh.patel2@mail.mcgill.ca>
2020-09-21 19:17:47 -0400

    Try queue now.

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,6 @@
 exports.run = (client, message, args, serverMusicQueue) => {
+    const serverMusicQueue = message.client.queue.get(message.guild.id)
+
     if (!message.member.voiceChannel){
         return message.channel.send(
             "You have to be in a voice channel to stop the music!"
@@ -7,6 +9,13 @@ exports.run = (client, message, args, serverMusicQueue) => {
     if (!serverMusicQueue) {
         return message.channel.send("There is no song that I could skip!");
     }
-    
-    serverMusicQueue.connection.dispatcher.end();
+    else{
+        try{
+            serverMusicQueue.playing = true
+            serverMusicQueue.connection.dispatcher.end();
+        }
+        catch(err){
+            console.log(err);
+        }
+    }
 }
--- commands/stop.js
+++ commands/stop.js
@@ -1,6 +1,13 @@
-exports.run = (client, message, args, serverMusicQueue) => {
+exports.run = (client, message, args) => {
+    const serverMusicQueue = message.client.queue.get(message.guild.id)
     if (!message.member.voiceChannel)
       return message.channel.send("You have to be in a voice channel to stop the music!");
+
+    try {
       serverMusicQueue.songs = [];
-    serverMusicQueue.connection.dispatcher.end();
+      serverMusicQueue.connection.dispatcher.end();
+    }
+    catch(err){
+      console.log(err);
+    }
 }
