064b4dea9bce72a7f937edc9ca54fc510ab453ca
Harsh Patel <harsh.patel2@mail.mcgill.ca>
2020-09-21 20:02:58 -0400

    Added queue command

---
+++ commands/queue.js
@@ -0,0 +1,24 @@
+const {MessageEmbed} = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+    const serverMusicQueue = message.client.queue.get(message.guild.id);
+
+    let fields = [];
+    i = 0;
+    serverMusicQueue.forEach(song => {
+        fields.append({name: `${i}) ${song.title}`, value: song.url})
+    });
+    const embed = new MessageEmbed()
+    // Set the title of the field
+    .setTitle('Current Music Queue')
+    // Set the color of the embed
+    .setColor(0xff0000)
+    // Set the main content of the embed
+    .setDescription('Hello sir, this is the current music queue you requested.')
+    .addFields(fields
+    )
+    .setTimestamp();
+
+    message.channel.send("pong!").catch(console.error);
+}
