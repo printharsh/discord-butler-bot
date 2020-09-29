d561dcd6957703367ac97215e009b58e91d8a030
Harsh Patel <me@mrharshpatel.com>
2020-09-29 16:02:52 -0400

    Create shuffle.js

---
+++ commands/shuffle.js
@@ -0,0 +1,18 @@
+exports.run = (client, message, args) => {
+    try{
+        const serverMusicQueue = client.queue.get(message.guild.id);
+
+        if(serverMusicQueue.shuffle){
+          message.channel.send('Sir, I am extremely sad that you do not enjoy my SHUFFLE.');
+          serverMusicQueue.shuffle = false;
+        }
+        else{
+          message.channel.send('Seems like you're feeling frisky today sir, time to SHUFFLE.');
+          serverMusicQueue.shuffle = true;
+        }
+    }
+    catch(err){
+        console.log(err)
+        message.channel.send('Sir, you are tripping there is nothing for to shuffle.')
+    }
+}
