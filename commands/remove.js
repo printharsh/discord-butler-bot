54cfc712f87c1b948ceb845899d06c1ec43a0764
Harsh Patel <me@mrharshpatel.com>
2020-09-29 16:31:10 -0400

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -1,26 +1,26 @@
 exports.run = (client, message, args) => {
-    const serverMusicQueue = message.client.queue.get(message.guild.id)
-    let numberToRemove = args[0] - 1
-    if(numberToRemove < 0 || numberToRemove > serverMusicQueue.songs.length){
-      return message.channel.send(
-            "Sir, I humbly request you to stop with these absurd requests. It's !remove {index of song to remove}."
-          );
-    }
-    if (!serverMusicQueue) {
-        return message.channel.send("There is nothing in queue to remove.");
-    }
-    else{
-        try{
-            let deletedSong = serverMusicQueue.songs.splice(numberToRemove, 1)
-            if(numberToRemove == 0){
-                // Stop playing because we are removing 1st song in queue.
-                serverMusicQueue.playing = true
-                serverMusicQueue.connection.dispatcher.end();
-            }
-            return message.channel.send(`As per your request, I have removed ${deletedSong[0].title}`);
+    try{
+        const serverMusicQueue = message.client.queue.get(message.guild.id)
+        let numberToRemove = args[0] - 1
+        if(numberToRemove < 0 || numberToRemove > serverMusicQueue.songs.length){
+          return message.channel.send(
+                "Sir, I humbly request you to stop with these absurd requests. It's !remove {index of song to remove}."
+              );
         }
-        catch(err){
-            console.log(err);
+        if (!serverMusicQueue) {
+            return message.channel.send("There is nothing in queue to remove.");
         }
+        else{
+                let deletedSong = serverMusicQueue.songs.splice(numberToRemove, 1)
+                if(numberToRemove == 0){
+                    // Stop playing because we are removing 1st song in queue.
+                    serverMusicQueue.playing = true
+                    serverMusicQueue.connection.dispatcher.end();
+                }
+                return message.channel.send(`As per your request, I have removed ${deletedSong[0].title}`);
+        }
+    }
+    catch(err){
+        console.log(err);
     }
 }
