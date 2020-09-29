abe5c44b01c365d4ec5d6773e407ae783eac7cb0
Harsh Patel <me@mrharshpatel.com>
2020-09-29 16:42:16 -0400

    Added shuffle function.

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,15 @@
 const ytdl = require("ytdl-core");
 const yts = require("yt-search");
 
+function shuffle(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
 function play(message, song) {
     const serverMusicQueue = message.client.queue.get(message.guild.id)
     console.log(serverMusicQueue)
